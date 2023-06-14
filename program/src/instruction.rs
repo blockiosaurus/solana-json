727f91c54ec88b833e56459176c0c76ffdb71249
blockiosaurus <blockiosaurus@gmail.com>
2023-06-14 14:11:50 -0400

    Adding instructions.

--- program/src/instruction.rs
+++ program/src/instruction.rs
@@ -1,6 +1,10 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 use shank::ShankInstruction;
-use solana_program::pubkey::Pubkey;
+use solana_program::{
+    instruction::{AccountMeta, Instruction},
+    pubkey::Pubkey,
+    system_program,
+};
 
 #[repr(C)]
 #[derive(PartialEq, Eq, Debug, Clone, BorshSerialize, BorshDeserialize)]
@@ -67,4 +71,122 @@ pub enum OnchainMetadataInstructions {
     #[account(1, writable, signer, name="payer", desc="The account that will pay for the transaction and rent.")]
     #[account(2, name="system_program", desc = "System program")]
     RemoveAuthority(RemoveAuthorityArgs),
+}
+
+pub fn initialize(
+    _program_id: Pubkey,
+    json_account: Pubkey,
+    json_metadata_account: Pubkey,
+    payer: Pubkey,
+) -> Instruction {
+    Instruction {
+        program_id: crate::ID,
+        accounts: vec![
+            AccountMeta::new(json_account, true),
+            AccountMeta::new(json_metadata_account, false),
+            AccountMeta::new(payer, true),
+            AccountMeta::new_readonly(system_program::ID, false),
+        ],
+        data: OnchainMetadataInstructions::Initialize
+            .try_to_vec()
+            .unwrap(),
+    }
+}
+
+pub fn close(
+    _program_id: Pubkey,
+    json_account: Pubkey,
+    json_metadata_account: Pubkey,
+    payer: Pubkey,
+) -> Instruction {
+    Instruction {
+        program_id: crate::ID,
+        accounts: vec![
+            AccountMeta::new(json_account, false),
+            AccountMeta::new(json_metadata_account, false),
+            AccountMeta::new(payer, true),
+            AccountMeta::new_readonly(system_program::ID, false),
+        ],
+        data: OnchainMetadataInstructions::Close.try_to_vec().unwrap(),
+    }
+}
+
+pub fn set_value(
+    _program_id: Pubkey,
+    json_account: Pubkey,
+    json_metadata_account: Pubkey,
+    payer: Pubkey,
+    args: SetValueArgs,
+) -> Instruction {
+    Instruction {
+        program_id: crate::ID,
+        accounts: vec![
+            AccountMeta::new(json_account, false),
+            AccountMeta::new(json_metadata_account, false),
+            AccountMeta::new(payer, true),
+            AccountMeta::new_readonly(system_program::ID, false),
+        ],
+        data: OnchainMetadataInstructions::SetValue(args)
+            .try_to_vec()
+            .unwrap(),
+    }
+}
+
+pub fn append_value(
+    _program_id: Pubkey,
+    json_account: Pubkey,
+    json_metadata_account: Pubkey,
+    payer: Pubkey,
+    args: AppendValueArgs,
+) -> Instruction {
+    Instruction {
+        program_id: crate::ID,
+        accounts: vec![
+            AccountMeta::new(json_account, false),
+            AccountMeta::new(json_metadata_account, false),
+            AccountMeta::new(payer, true),
+            AccountMeta::new_readonly(system_program::ID, false),
+        ],
+        data: OnchainMetadataInstructions::AppendValue(args)
+            .try_to_vec()
+            .unwrap(),
+    }
+}
+
+pub fn add_authority(
+    _program_id: Pubkey,
+    json_metadata_account: Pubkey,
+    payer: Pubkey,
+    args: AddAuthorityArgs,
+) -> Instruction {
+    Instruction {
+        program_id: crate::ID,
+        accounts: vec![
+            AccountMeta::new(json_metadata_account, false),
+            AccountMeta::new(payer, true),
+            AccountMeta::new_readonly(system_program::ID, false),
+        ],
+        data: OnchainMetadataInstructions::AddAuthority(args)
+            .try_to_vec()
+            .unwrap(),
+    }
+}
+
+pub fn remove_authority(
+    _program_id: Pubkey,
+    json_metadata_account: Pubkey,
+    payer: Pubkey,
+    args: RemoveAuthorityArgs,
+) -> Instruction {
+    Instruction {
+        program_id: crate::ID,
+        accounts: vec![
+            AccountMeta::new(json_metadata_account, false),
+            AccountMeta::new(payer, true),
+            AccountMeta::new_readonly(system_program::ID, false),
+        ],
+        data: OnchainMetadataInstructions::RemoveAuthority(args)
+            .try_to_vec()
+            .unwrap(),
+    }
 }
